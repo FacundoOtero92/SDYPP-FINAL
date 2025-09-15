00fff7b94acf5b3028e779132079af9cfd9f3765
Facundo Otero <facundo_otero@hotmail.com>
2025-09-15 02:44:25 -0300

    tfs4

--- Infra/terraform/11-worker-cpu.tf
+++ Infra/terraform/11-worker-cpu.tf
@@ -1,56 +1,112 @@
-resource "google_compute_instance" "pruebavm" {
-  count         = var.instancias
-  name          = "worker-cpu-${count.index}"
-  machine_type  = var.tipo_vm
-  zone          = var.zone
+# resource "google_compute_instance" "pruebavm" {
+#   count         = var.instancias
+#   name          = "worker-cpu-${count.index}"
+#   machine_type  = var.tipo_vm
+#   zone          = var.zone
 
-  boot_disk {
-    initialize_params {
-      image = var.imagen
-    }
+#   boot_disk {
+#     initialize_params {
+#       image = var.imagen
+#     }
+#   }
+
+# locals {
+#   startup_script = templatefile("${path.module}/startup.sh.tftpl", {
+#     worker_env   = var.worker_env
+#     worker_image = var.worker_image
+#   })
+# }
+#   network_interface {
+#     network = "default"
+#     access_config {}
+#   }
+
+#  metadata = {
+#     startup-script = local.startup_script
+#   }
+
+ 
+#   # SA por si luego querés API GCP desde el container (opcional)
+#   service_account {
+#     scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+#   }
+# }
+
+# # Managed Instance Group (el “pool”)
+# resource "google_compute_instance_group_manager" "worker_cpu_mig" {
+#   name               = "worker-cpu-mig"
+#   base_instance_name = "worker-cpu"
+#   zone               = var.zone
+
+#   version {
+#     instance_template = google_compute_instance_template.worker_cpu_tpl.self_link
+#   }
+
+#    target_size = var.pool_size   #   tamaño del pool (lo cambiás con terraform apply)
+
+#   update_policy {
+#     minimal_action          = "REPLACE"
+#     type                    = "PROACTIVE"
+#     max_surge_fixed         = 1
+#     max_unavailable_fixed   = 0
+#   }
+# }
+
+# output "pool_name"  { value = google_compute_instance_group_manager.worker_cpu_mig.name }
+# output "tpl_name"   { value = google_compute_instance_template.worker_cpu_tpl.name }
+# Instance Template para el pool
+resource "google_compute_instance_template" "worker_cpu_tpl" {
+  name_prefix  = "worker-cpu-${count.index}"
+  machine_type = var.machine_type
+
+  disk {
+    boot         = true
+    auto_delete  = true
+    source_image = var.imagen
+    disk_size_gb = 20
   }
 
-locals {
-  startup_script = templatefile("${path.module}/startup.sh.tftpl", {
-    worker_env   = var.worker_env
-    worker_image = var.worker_image
-  })
-}
   network_interface {
-    network = "default"
-    access_config {}
+    network = var.network
+    access_config {}  # IP pública para docker pull (si no usás NAT)
   }
 
- metadata = {
+  tags = ["worker-cpu"]
+
+  metadata = {
     startup-script = local.startup_script
   }
 
- 
-  # SA por si luego querés API GCP desde el container (opcional)
+  scheduling {
+    preemptible        = var.use_spot
+    provisioning_model = var.use_spot ? "SPOT" : "STANDARD"
+    automatic_restart  = var.use_spot ? false : true
+  }
+
   service_account {
     scopes = ["https://www.googleapis.com/auth/cloud-platform"]
   }
 }
 
-# Managed Instance Group (el “pool”)
+# Managed Instance Group (pool)
 resource "google_compute_instance_group_manager" "worker_cpu_mig" {
   name               = "worker-cpu-mig"
-  base_instance_name = "worker-cpu"
+  base_instance_name = "worker-cpu-${count.index}"
   zone               = var.zone
 
   version {
     instance_template = google_compute_instance_template.worker_cpu_tpl.self_link
   }
 
-   target_size = var.pool_size   #   tamaño del pool (lo cambiás con terraform apply)
+  target_size = var.pool_size
 
   update_policy {
-    minimal_action          = "REPLACE"
-    type                    = "PROACTIVE"
-    max_surge_fixed         = 1
-    max_unavailable_fixed   = 0
+    minimal_action        = "REPLACE"
+    type                  = "PROACTIVE"
+    max_surge_fixed       = 1
+    max_unavailable_fixed = 0
   }
 }
 
-output "pool_name"  { value = google_compute_instance_group_manager.worker_cpu_mig.name }
-output "tpl_name"   { value = google_compute_instance_template.worker_cpu_tpl.name }
+output "pool_name" { value = google_compute_instance_group_manager.worker_cpu_mig.name }
+output "tpl_name"  { value = google_compute_instance_template.worker_cpu_tpl.name }
